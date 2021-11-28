cc26d21e071d3816a6aa121ef4373e8e4bf1240f
kimyoungjae <sskim5421@gmail.com>
2021-11-28 18:54:56 +0900

    Add screens frame & LogContext

---
+++ src/screens/RootStack.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import MainTab from './MainTab';
+import WriteScreen from './WriteScreen';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="MainTab"
+        component={MainTab}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen name="Write" component={WriteScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default RootStack;
