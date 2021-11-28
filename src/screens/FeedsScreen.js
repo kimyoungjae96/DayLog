c7d472e35a4f3839d32f158e27b9837abc7b6117
kimyoungjae <sskim5421@gmail.com>
2021-11-28 19:06:12 +0900

    Add floatingWriteButton component

--- src/screens/FeedsScreen.js
+++ src/screens/FeedsScreen.js
@@ -1,23 +1,19 @@
 import React, {useContext} from 'react';
 import {StyleSheet, View, TextInput} from 'react-native';
 import LogContext from '../contexts/LogContext';
+import FloatingWriteButton from '../components/FloatingWriteButton';
 
 function FeedsScreen() {
   const {text, setText} = useContext(LogContext);
   return (
     <View style={styles.block}>
-      <TextInput
-        value={text}
-        onChangeText={setText}
-        placeholder="텍스트를 입력하세요."
-        style={styles.input}
-      />
+      <FloatingWriteButton />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  block: {},
+  block: {flex: 1},
   box: {
     borderWidth: 2,
     padding: 16,
