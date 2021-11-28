92d164988aac2ae312f4eb4eb8c8ca3607461511
kimyoungjae <sskim5421@gmail.com>
2021-11-28 19:56:13 +0900

    Add keyboard avoiding view in write screen

--- src/screens/WriteScreen.js
+++ src/screens/WriteScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {KeyboardAvoidingView, Platform, StyleSheet} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import WriteHeader from '../components/WriteHeader';
 import WriteEditor from '../components/WriteEditor';
@@ -7,14 +7,21 @@ import WriteEditor from '../components/WriteEditor';
 function WriteScreen() {
   return (
     <SafeAreaView style={styles.block}>
-      <WriteHeader />
-      <WriteEditor />
+      <KeyboardAvoidingView
+        style={styles.avoidingView}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+        <WriteHeader />
+        <WriteEditor />
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   block: {flex: 1, backgroundColor: 'white'},
+  avoidView: {
+    flex: 1,
+  },
 });
 
 export default WriteScreen;
