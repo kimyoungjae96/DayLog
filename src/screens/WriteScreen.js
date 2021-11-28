47c19bb2b5b6eeed75a815ec9668c12942be5477
kimyoungjae <sskim5421@gmail.com>
2021-11-28 19:54:14 +0900

    Add write editor

--- src/screens/WriteScreen.js
+++ src/screens/WriteScreen.js
@@ -2,11 +2,13 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import WriteHeader from '../components/WriteHeader';
+import WriteEditor from '../components/WriteEditor';
 
 function WriteScreen() {
   return (
     <SafeAreaView style={styles.block}>
       <WriteHeader />
+      <WriteEditor />
     </SafeAreaView>
   );
 }
