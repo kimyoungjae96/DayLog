fe0e78bf22eeba844e1b83dc3ba2c513391a8517
kimyoungjae <sskim5421@gmail.com>
2021-12-02 20:29:02 +0900

    Add remove & modify on Log

--- src/components/FeedListItem.js
+++ src/components/FeedListItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Platform, Pressable, StyleSheet, Text} from 'react-native';
 import {format, formatDistanceToNow} from 'date-fns';
 import {ko} from 'date-fns/locale';
+import {useNavigation} from '@react-navigation/native';
 
 function formatDate(date) {
   const d = new Date(date);
@@ -29,6 +30,13 @@ function truncate(text) {
 
 function FeedListItem({log}) {
   const {title, body, date} = log;
+  const navigation = useNavigation();
+
+  const onPress = () => {
+    navigation.navigate('Write', {
+      log,
+    });
+  };
 
   return (
     <Pressable
@@ -36,6 +44,7 @@ function FeedListItem({log}) {
         styles.block,
         Platform.OS === 'ios' && pressed && {backgroundColor: '#efefef'},
       ]}
+      onPress={onPress}
       android_ripple={{color: '#ededed'}}>
       <Text style={styles.date}>{formatDate(date)}</Text>
       <Text style={styles.title}>{title}</Text>
