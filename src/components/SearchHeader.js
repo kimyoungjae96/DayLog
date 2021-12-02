d38891163a7212b7c12c32ad765131072b6f1514
kimyoungjae <sskim5421@gmail.com>
2021-12-02 20:39:52 +0900

    Add search component

---
+++ src/components/SearchHeader.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Pressable,
+  useWindowDimensions,
+  TextInput,
+  View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+function SearchHeader() {
+  const {width} = useWindowDimensions();
+  return (
+    <View style={[styles.block, {width: width - 32}]}>
+      <TextInput
+        style={styles.input}
+        placeholder="검색어를 입력하세요"
+        autoFocus
+      />
+      <Pressable
+        style={({pressed}) => [styles.button, pressed && {opacity: 0.5}]}>
+        <Icon name="cancel" size={20} color={'#9e9e9e'} />
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+  },
+});
+
+export default SearchHeader;
