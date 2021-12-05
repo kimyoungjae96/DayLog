8689e2295000f2c68d8db4c90e4e69f986ccd775
kimyoungjae <sskim5421@gmail.com>
2021-12-05 16:34:19 +0900

    Add calendar dependency & CalendarView

--- src/screens/CalendarScreen.js
+++ src/screens/CalendarScreen.js
@@ -1,22 +1,8 @@
-import React, {useContext} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-import LogContext from '../contexts/LogContext';
+import React from 'react';
+import CalendarView from '../components/CalendarView';
 
 function CalendarScreen() {
-  const {text} = useContext(LogContext);
-  return (
-    <View style={styles.block}>
-      <Text style={styles.text}>{text}</Text>
-    </View>
-  );
+  return <CalendarView />;
 }
-
-const styles = StyleSheet.create({
-  block: {},
-  text: {
-    padding: 16,
-    fontSize: 24,
-  },
-});
 
 export default CalendarScreen;
