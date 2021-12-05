d62840d86f7a724f48e7c86763cbb9bf763eceb7
kimyoungjae <sskim5421@gmail.com>
2021-12-05 16:52:46 +0900

    Add CalendarView in FeedList

--- src/components/CalendarView.js
+++ src/components/CalendarView.js
@@ -2,8 +2,29 @@ import React from 'react';
 import {Calendar} from 'react-native-calendars';
 import {StyleSheet} from 'react-native';
 
-function CalendarView() {
-  return <Calendar style={styles.calendar} />;
+function CalendarView({markedDates, selectedDate, onSelectDate}) {
+  const markedSelectedDates = {
+    ...markedDates,
+    [selectedDate]: {
+      selected: true,
+      marked: markedDates[selectedDate]?.marked,
+    },
+  };
+  return (
+    <Calendar
+      style={styles.calendar}
+      markedDates={markedSelectedDates}
+      onDayPress={day => {
+        onSelectDate(day.dateString);
+      }}
+      theme={{
+        selectedDayBackgroundColor: '#009688',
+        arrowColor: '#009688',
+        dotColor: '#009688',
+        todayTextColor: '#009688',
+      }}
+    />
+  );
 }
 
 const styles = StyleSheet.create({
--- src/screens/CalendarScreen.js
+++ src/screens/CalendarScreen.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import {format} from 'date-fns';
+import React, {useState, useContext} from 'react';
 import CalendarView from '../components/CalendarView';
+import LogContext from '../contexts/LogContext';
+import FeedList from '../components/FeedList';
 
 function CalendarScreen() {
-  return <CalendarView />;
+  const {logs} = useContext(LogContext);
+  const [selectedDate, setSelectedDate] = useState(
+    format(new Date(), 'yyyy-MM-dd'),
+  );
+
+  const markedDates = logs.reduce((acc, current) => {
+    const formattedDate = format(new Date(current.date), 'yyyy-MM-dd');
+    acc[formattedDate] = {marked: true};
+    return acc;
+  }, {});
+
+  const filteredLogs = logs.filter(
+    log => format(new Date(log.date), 'yyyy-MM-dd') === selectedDate,
+  );
+
+  return (
+    <FeedList
+      logs={filteredLogs}
+      ListHeaderComponent={
+        <CalendarView
+          markedDates={markedDates}
+          selectedDate={selectedDate}
+          onSelectDate={setSelectedDate}
+        />
+      }
+    />
+  );
 }
 
 export default CalendarScreen;
