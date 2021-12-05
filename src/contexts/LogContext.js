43d2bbdfe531b7f0a1ea547840ad6b46a60af9a9
kimyoungjae <sskim5421@gmail.com>
2021-12-05 17:53:35 +0900

    Save logs in async storage

--- src/contexts/LogContext.js
+++ src/contexts/LogContext.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import {createContext, useState} from 'react';
 import {v4 as uuidv4} from 'uuid';
+import logsStorage from '../storages/localStorage';
 
 const LogContext = createContext('안녕하세요');
 
 export function LogContextProvider({children}) {
+  const initialLogsRef = useRef(null);
   const [logs, setLogs] = useState(
     Array.from({length: 10})
       .map((_, index) => ({
@@ -35,6 +37,24 @@ export function LogContextProvider({children}) {
     const nextLogs = logs.filter(log => log.id !== id);
     setLogs(nextLogs);
   };
+
+  useEffect(() => {
+    (async () => {
+      const savedLogs = await logsStorage.get();
+      if (savedLogs) {
+        initialLogsRef.current = savedLogs;
+        setLogs(savedLogs);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (logs === initialLogsRef.current) {
+      return;
+    }
+    logsStorage.set(logs);
+  }, [logs]);
+
   return (
     <LogContext.Provider value={{logs, onCreate, onModify, onRemove}}>
       {children}
